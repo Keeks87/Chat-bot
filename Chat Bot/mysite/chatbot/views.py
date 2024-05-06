23ff0ae2a4782b40058fd1020e764cae3639b78b
Keenan <110526824+Keeks87@users.noreply.github.com>
2024-05-06 09:46:24 +0200

    Backend Integration

--- Chat Bot/mysite/chatbot/views.py
+++ Chat Bot/mysite/chatbot/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .bot import get_bot_response
 
-# Create your views here.
+def chat_response(request):
+    user_input = request.GET.get('message')
+    return HttpResponse(get_bot_response(user_input))
